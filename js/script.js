7bafbc9c4be04f26e6738ad51fb1d44f268b66a0
Hunter MacDermut <whosebluesanyway@gmail.com>
2014-04-27 02:51:11 -0400

    Started work on JS.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,57 @@
-$(document).ready(function() {
+var a1 = document.getElementById("a1");
+var a2 = document.getElementById("a2");
+var a3 = document.getElementById("a3");
+var a4 = document.getElementById("a4");
+var a5 = document.getElementById("a5");
 
-});
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
+var b3 = document.getElementById("b3");
+var b4 = document.getElementById("b4");
+var b5 = document.getElementById("b5");
+
+var c1 = document.getElementById("c1");
+var c2 = document.getElementById("c2");
+var c3 = document.getElementById("c3");
+var c4 = document.getElementById("c4");
+var c5 = document.getElementById("c5");
+
+var d1 = document.getElementById("d1");
+var d2 = document.getElementById("d2");
+var d3 = document.getElementById("d3");
+var d4 = document.getElementById("d4");
+var d5 = document.getElementById("d5");
+
+var e1 = document.getElementById("e1");
+var e2 = document.getElementById("e2");
+var e3 = document.getElementById("e3");
+var e4 = document.getElementById("e4");
+var e5 = document.getElementById("e5");
+
+var f1 = document.getElementById("f1");
+var f2 = document.getElementById("f2");
+var f3 = document.getElementById("f3");
+var f4 = document.getElementById("f4");
+var f5 = document.getElementById("f5");
+
+var g1 = document.getElementById("g1");
+var g2 = document.getElementById("g2");
+var g3 = document.getElementById("g3");
+var g4 = document.getElementById("g4");
+var g5 = document.getElementById("g5");
+
+var h1 = document.getElementById("h1");
+var h2 = document.getElementById("h2");
+var h3 = document.getElementById("h3");
+var h4 = document.getElementById("h4");
+var h5 = document.getElementById("h5");
+
+var i1 = document.getElementById("i1");
+var i2 = document.getElementById("i2");
+var i3 = document.getElementById("i3");
+var i4 = document.getElementById("i4");
+var i5 = document.getElementById("i5");
+
+function oneUp(bead) {
+	bead.style.top = ""
+}
